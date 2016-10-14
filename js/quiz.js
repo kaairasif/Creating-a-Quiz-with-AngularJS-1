91a76d7a2abd40388d9348fb4c35786409225870
asif <asifur.rahman@adpeople.com>
2016-10-14 22:30:24 +0600

    MAnipulating the HTML on json data

--- js/quiz.js
+++ js/quiz.js
@@ -14,6 +14,33 @@
 			$scope.totalQuestions = $scope.myQuestions.length;
 		});
 
-	}]);
+		$scope.selectAnswer = function(qIndex, aIndex) {
+			
+			var questionState =  $scope.myQuestions[qIndex].questionState;
+			
+			if ( questionState != 'answered' ) {
+				$scope.myQuestions[qIndex].selectedAnswer = aIndex;
+				var correctAnswer = $scope.myQuestions[qIndex].correct;
 
+				if ( aIndex === correctAnswer ) {
+					$scope.myQuestions[qIndex].correctness = 'correct';
+					$scope.score += 1;
+				} else {
+					$scope.myQuestions[qIndex].correctness = 'incorrect';
+				}
+				$scope.myQuestions[qIndex].questionState = 'answered';
+			}
+		} //end click
+
+		$scope.isSelected = function(qIndex, aIndex){
+			if($scope.myQuestions[qIndex].correctAnswer){
+				console.log("hi");
+			}
+			return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+		}
+		$scope.isCorrect = function(qIndex, aIndex){
+			
+			return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+		}
+	}]);
 })();
