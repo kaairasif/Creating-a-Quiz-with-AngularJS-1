434853918edbd85621bb6037a61fb0bbde670d55
asif <asifcse22@gmail.com>
2016-10-16 14:31:48 -0700

    Result state email and share to friends and others

--- js/quiz.js
+++ js/quiz.js
@@ -30,6 +30,8 @@
 				}
 				$scope.myQuestions[qIndex].questionState = 'answered';
 			}
+			$scope.percentage = (($scope.score / $scope.totalQuestions)*100).toFixed(1);
+
 		} //end click
 
 		$scope.isSelected = function(qIndex, aIndex){
@@ -44,6 +46,22 @@
 		}
 		$scope.selectContinue = function() {
 			return $scope.activeQuestion += 1;
+		}
+
+		$scope.createShareLinks = function(percentage) {
+			var url = 'http://kaairasif.com';
+
+			var emailLink = '<a class="btn email" href="mailto:?subject=Try to beat my quiz score!&amp;body=I scored 
+			a ' + percentage + ' % on this quiz about Saturn. try to beat my score at '+url+'"> Email a friend </a>';
+			
+			var twitterLink = '<a class="btn twitter" target="_blank" href="http://twitter.com/share?text=I scored a
+			' + percentage + ' % on this quiz about Saturn. Try to beat my score at&amp;hashtags=SaturnQuiz&amp;url= ' + url + ' ">Tweet your score </a>';
+
+
+			var newMarkup = emailLink + twitterLink;
+
+			return $sce.trustAsHtml(newMarkup);	
+
 		}
 
 
