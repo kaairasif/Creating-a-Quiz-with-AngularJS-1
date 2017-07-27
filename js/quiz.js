42a20b4da4fe874ab5910cb2d1d7a33b638f6cf8
asif <asifcse22@gmail.com>
2017-07-27 07:57:42 +0600

    update

--- js/quiz.js
+++ js/quiz.js
@@ -1,21 +1,19 @@
 (function(){
 
 	var app = angular.module('myQuiz', []);
-
-	app.controller('QuizController', ['$scope','$http','$sce', function($scope, $http, $sce){
+		app.controller('QuizController', ['$scope','$http','$sce', function($scope, $http, $sce) {
 
 		$scope.score = 0;
 		$scope.activeQuestion = -1;
 		$scope.activeQuestionAnswered = 0;
 		$scope.percentage = 0;
 
-		$http.get('quiz_data.json').then(function(quizData){
+		$http.get('quiz_data.json').then(function(quizData) {
 			$scope.myQuestions = quizData.data;
 			$scope.totalQuestions = $scope.myQuestions.length;
 		});
 
 		$scope.selectAnswer = function(qIndex, aIndex) {
-
 			var questionState =  $scope.myQuestions[qIndex].questionState;			
 			if ( questionState != 'answered' ) {
 				$scope.myQuestions[qIndex].selectedAnswer = aIndex;
@@ -31,14 +29,13 @@
 				$scope.myQuestions[qIndex].questionState = 'answered';
 			}
 			$scope.percentage = (($scope.score / $scope.totalQuestions)*100).toFixed(1);
-
 		} //end click
 
-		$scope.isSelected = function(qIndex, aIndex){
+		$scope.isSelected = function(qIndex, aIndex) {
 			return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
 		}
 
-		$scope.isCorrect = function(qIndex, aIndex){			
+		$scope.isCorrect = function(qIndex, aIndex) {			
 			return $scope.myQuestions[qIndex].correctAnswer === aIndex;
 		}
 
@@ -48,19 +45,13 @@
 
 		$scope.createShareLinks = function(percentage) {
 			var url = 'http://kaairasif.com/angular_quiz';
-
 			var emailLink = '<a class="btn email" href="mailto:?subject=Try to beat my quiz score!&amp;body=I scored a ' + percentage + ' % on this quiz about Saturn. try to beat my score at ' + url + ' "> Email a friend </a>';
-		
 			var myTwitt =  encodeURIComponent('I scored a ' + percentage + ' % on this quiz about Saturn. Try to beat my score at ' + url);
-
 			var twitterLink = '<a class="btn twitter" target="_blank" href="http://twitter.com/share?text=' + myTwitt +'">Tweet your score </a>';
-
 			var newMarkup = emailLink + twitterLink;
-
 			return $sce.trustAsHtml(newMarkup);	
-
 		}
-
 
 	}]);
 })();
+
